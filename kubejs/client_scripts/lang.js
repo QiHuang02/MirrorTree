ecb1b701ea88a13e73e7db9e7054bc2fbbc8ac29
QiHuang02 <2830447227@qq.com>
2024-09-16 11:03:29 +0800

    添加lang来支持本地化

---
+++ kubejs/client_scripts/lang.js
@@ -0,0 +1,29 @@
+let blockResourceLang = [
+    [`${global.packid}.solar_panel`, "太阳能发电机", "Solar Panel"],
+    [`${global.packid}.dust`, "尘土", "Dust"]
+];
+let itemResourceLang = [
+    [`${global.packid}.wooden_hammer`, "木 锤", "Wooden Hammer"],
+    [`${global.packid}.cobblestone_hammer`, "石 锤", "Cobblestone Hammer"],
+    [`${global.packid}.iron_hammer`, "铁 锤", "Iron Hammer"],
+    [`${global.packid}.diamond_hammer`, "钻石 锤", "Diamond Hammer"],
+    [`${global.packid}.netherite_hammer`, "下界合金 锤", "Netherite Hammer"],
+    [`${global.packid}.infinity_upgrade_smithing_template`, "锻造模板", "Upgrade Smithing Template"]
+]
+
+blockResourceLang.forEach(([key, zh_cn, en_us]) => {
+    ClientEvents.lang('zh_cn', (event) => {
+        event.add('block.' + key, zh_cn);
+    });
+    ClientEvents.lang('en_us', (event) => {
+        event.add('block.' + key, en_us);
+    })
+});
+itemResourceLang.forEach(([key, zh_cn, en_us]) => {
+    ClientEvents.lang('zh_cn', (event) => {
+        event.add('item.' + key, zh_cn);
+    });
+    ClientEvents.lang('en_us', (event) => {
+        event.add('item.' + key, en_us);
+    })
+});
